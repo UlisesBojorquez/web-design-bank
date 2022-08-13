e5cbd1e96a61730279f9186fdaf11a0f2219eb75
Ulises Bojorquez Ortiz <ulibojorquez@gmail.com>
2022-08-13 14:38:41 -0500

    File Structure and design template

--- web_design_bank/src/App.jsx
+++ web_design_bank/src/App.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import styles from './style'
+import { 
+  Navbar, 
+  Hero, 
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer 
+} from './components'
 
-const App = () => {
-  return (
-    <div>App</div>
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      {/* Navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+      {/* Hero Banner */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+      {/* Statistics */}
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+
+        </div>
+      </div>
+    </div>
   )
-}
 
 export default App
---
+++ web_design_bank/src/components/index.js
@@ -0,0 +1,39 @@
+export { default as Billing} from './Billing'
+export { default as Business} from './Business'
+export { default as Button} from './Button'
+export { default as CardDeal} from './CardDeal'
+export { default as Clients} from './Clients'
+export { default as CTA} from './CTA'
+export { default as FeedBack} from './FeedBack'
+export { default as Footer} from './Footer'
+export { default as GetStarted} from './GetStarted'
+export { default as Hero} from './Hero'
+export { default as Navbar} from './Navbar'
+export { default as Stats} from './Stats'
+export { default as Testimonials} from './Testimonials'
+
+/*
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
+*/
